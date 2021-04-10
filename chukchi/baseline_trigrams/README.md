9da926655ef768e49d208625e30593a438c95f5c
Olga Bystrova <bystrovaolgavl@gmail.com>
2021-04-10 16:42:32 +0300

    added trigrams baseline model

---
+++ chukchi/baseline_trigrams/README.md
@@ -0,0 +1,37 @@
+# Baseline system
+
+This is a simple next-word prediction system using bigram probabilities. For each word in the input it 
+tries to predict the next word and falls back to predicting the most frequent unigram.
+
+You can train the system by using:
+
+```bash
+$ python3 train.py ../data/train.tsv model.dat
+Written 33724 unigrams and 107835 bigrams to model.dat.
+```
+
+And then you can run it using:
+
+```bash
+$ python3 predict.py model.dat < ../data/dev.tsv > output.tsv
+```
+
+RESULTS 
+
+```bash
+$ python3 ../evaluate.py ../data/dev.tsv output.tsv 
+Characters: 37927
+Tokens: 8374
+Clicks: 30340
+Clicks/Token: 3.6231191784093624
+Clicks/Character: 0.7999578136947294
+```
+
+Results with concatenation dev + train
+```bash
+Characters: 37927
+Tokens: 8374
+Clicks: 30363
+Clicks/Token: 3.6258657750179126
+Clicks/Character: 0.800564241832995
+```
---
+++ chukchi/baseline_trigrams/predict.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys, pickle
+
+# This is the filename of the model file
+model_file = sys.argv[1]
+mf = open(model_file, 'rb')
+
+# Load the unigram and bigram probabilities from the model file
+(unigrams, bigrams, trigrams) = pickle.load(mf, encoding='utf-8')
+
+# Initialise the probability of the start of the sentence.
+unigrams['#'] = 0.0
+bigrams['#']['#'] = 0.0
+# Hits is number of times we get a prediction right
+hits = 0
+# The number of tokens
+n_tokens = 0
+# For each of the lines in the input
+for line in sys.stdin.readlines():
+    # Split into two columns
+    row = line.strip().split('\t')
+    # Our tokens are in column one, split by space
+    tokens = row[0].split(' ')
+    # The test tokens are the beginning of sentence symbol + the list of tokens
+    tst_tokens = ['#', '#'] + tokens
+    # Increment the number of tokens by the length of the list containing the tokens
+    n_tokens += len(tokens)
+
+    # This is our output
+    output = []
+    # For each of the tokens in the "tst_tokens" list (e.g. the list + the beginning of sentence symbol)
+    for i in range(len(tst_tokens) - 2):
+        up = 0.0  # Unigram probability
+        bp = 0.0  # Bigram probability
+        tp = 0.0  # Trigram probability
+        first = tst_tokens[i]  # First token in bigram
+        second = tst_tokens[i + 1]  # Second token in trigram
+        third = tst_tokens[i + 2]
+        # If we find the first token in the bigrams dict
+        if first in trigrams:
+            if second in trigrams[first]:
+                # We get the best prediction for what token should come next
+                pred = max(trigrams[first][second], key=trigrams[first][second].get)
+                # If it matches with what the next token really is
+                if pred == third:
+                    # We add this whole token to the output
+                    # e.g. a single click on a prediction
+                    output.append(pred)
+                    # Increment the number of hits by 1
+                    hits += 1
+                else:
+                    # Otherwise we add each individual character to the output
+                    # e.g. writing out each of the individual clicks
+                    output += [c for c in third]
+        # If we haven't found a trigram, we just try proposing the most frequent unigram
+        else:
+            # Find the highest scoring unigram
+            pred = max(unigrams, key=unigrams.get)
+            # If the prediction is right
+            if pred == third:
+                # Append to output
+                output.append(pred)
+                hits += 1
+            else:
+                # Otherwise append each individual character
+                output += [c for c in third]
+        # Finally append a space symbol
+        output.append('_')
+    # break
+    # Print out our input and the predicted sequence of keypresses
+    print('%s\t%s' % (row[0], ' '.join(output)))
+
+print('Hits:', hits, '; Tokens:', n_tokens, file=sys.stderr)
